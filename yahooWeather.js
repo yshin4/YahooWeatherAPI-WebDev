f905a1a1696c7c14295ad660ce177b3e35f37e18
yshin4 <yshin4@lion.lmu.edu>
2016-11-01 14:32:32 -0700

    edit on if statment, null to !

--- yahooWeather.js
+++ yahooWeather.js
@@ -5,7 +5,7 @@ function getForecast(){
   var location = $('#city').val();
 
   $.get('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '")&format=json', function(data){
-    if(data.query.results === null){
+    if(!data.query.results){
       alert("Location not found: " + location + "!");
     } else {
       $('.result-container').html('<h2>' + data.query.results.channel.item.title + '</h2>' + data.query.results.channel.item.description)
