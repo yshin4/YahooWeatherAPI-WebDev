8af50aa6bef533bb61102c03472505f172514223
jbreaker1 <jpatel8@lion.lmu.edu>
2016-11-01 15:31:48 -0700

    tried to add the wind portion

--- yahooWeather.js
+++ yahooWeather.js
@@ -4,7 +4,24 @@
 function getForecast(){
   var location = $('#city').val();
 
-  $.get('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '")&format=json', function(data){
+  $.get('https://query.yahooapis.com/v1/public/yql', {
+  	q: 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '")',
+  	format: 'json',
+  	u: 'c'
+  }, function(data){
+    if(!data.query.results){
+      alert("Location not found: " + location + "!");
+    } else {
+      $('.result-container').html('<h2>' + data.query.results.channel.item.title + '</h2>' + data.query.results.channel.item.description)
+    }
+  })
+  ;
+
+  $.get('https://query.yahooapis.com/v1/public/yql', {
+  	q: 'select wind from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '")',
+  	format: 'json',
+  	u: 'c'
+  }, function(data){
     if(!data.query.results){
       alert("Location not found: " + location + "!");
     } else {
