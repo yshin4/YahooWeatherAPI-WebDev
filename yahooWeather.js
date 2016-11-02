0aada65207878836254026bd660c5b3465ea1204
yshin4 <yshin4@lion.lmu.edu>
2016-11-01 22:54:29 -0700

    added buttons for forecast by day

--- yahooWeather.js
+++ yahooWeather.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 function getForecast(){
   var location = $('#city').val();
 
@@ -16,6 +17,36 @@ function getForecast(){
                                     + '<h4>' + "Current Condition: " + data.query.results.channel.item.forecast[0].text + '</h4>'
                                     + '<h4>' + "Low: " + data.query.results.channel.item.forecast[0].low + "°" + data.query.results.channel.units.temperature + '</h4>'
                                     + '<h4>' + "High: " + data.query.results.channel.item.forecast[0].high + "°" + data.query.results.channel.units.temperature + '</h4>');
+      $('.day1-day').html(data.query.results.channel.item.forecast[1].day);
+      $('.day2-day').html(data.query.results.channel.item.forecast[2].day);
+      $('.day3-day').html(data.query.results.channel.item.forecast[3].day);
+      $('.day4-day').html(data.query.results.channel.item.forecast[4].day);
+      $('.day5-day').html(data.query.results.channel.item.forecast[5].day);
+      $('.day6-day').html(data.query.results.channel.item.forecast[6].day);
+      $('.day1-forecast').html('<h3>' + "Forecast for " +  data.query.results.channel.item.forecast[1].day + ", " + data.query.results.channel.item.forecast[1].date + ":" + '</h3>'
+                                + '<h4>' + "Current Condition: " + data.query.results.channel.item.forecast[1].text + '</h4>'
+                                + '<h4>' + "Low: " + data.query.results.channel.item.forecast[1].low + "°" + data.query.results.channel.units.temperature + '</h4>'
+                                + '<h4>' + "High: " + data.query.results.channel.item.forecast[1].high + "°" + data.query.results.channel.units.temperature + '</h4>');
+      $('.day2-forecast').html('<h3>' + "Forecast for " +  data.query.results.channel.item.forecast[2].day + ", " + data.query.results.channel.item.forecast[1].date + ":" + '</h3>'
+                                + '<h4>' + "Current Condition: " + data.query.results.channel.item.forecast[2].text + '</h4>'
+                                + '<h4>' + "Low: " + data.query.results.channel.item.forecast[2].low + "°" + data.query.results.channel.units.temperature + '</h4>'
+                                + '<h4>' + "High: " + data.query.results.channel.item.forecast[2].high + "°" + data.query.results.channel.units.temperature + '</h4>');
+      $('.day3-forecast').html('<h3>' + "Forecast for " +  data.query.results.channel.item.forecast[3].day + ", " + data.query.results.channel.item.forecast[1].date + ":" + '</h3>'
+                                + '<h4>' + "Current Condition: " + data.query.results.channel.item.forecast[3].text + '</h4>'
+                                + '<h4>' + "Low: " + data.query.results.channel.item.forecast[3].low + "°" + data.query.results.channel.units.temperature + '</h4>'
+                                + '<h4>' + "High: " + data.query.results.channel.item.forecast[3].high + "°" + data.query.results.channel.units.temperature + '</h4>');
+      $('.day4-forecast').html('<h3>' + "Forecast for " +  data.query.results.channel.item.forecast[4].day + ", " + data.query.results.channel.item.forecast[1].date + ":" + '</h3>'
+                                + '<h4>' + "Current Condition: " + data.query.results.channel.item.forecast[4].text + '</h4>'
+                                + '<h4>' + "Low: " + data.query.results.channel.item.forecast[4].low + "°" + data.query.results.channel.units.temperature + '</h4>'
+                                + '<h4>' + "High: " + data.query.results.channel.item.forecast[4].high + "°" + data.query.results.channel.units.temperature + '</h4>');
+      $('.day5-forecast').html('<h3>' + "Forecast for " +  data.query.results.channel.item.forecast[5].day + ", " + data.query.results.channel.item.forecast[1].date + ":" + '</h3>'
+                                + '<h4>' + "Current Condition: " + data.query.results.channel.item.forecast[5].text + '</h4>'
+                                + '<h4>' + "Low: " + data.query.results.channel.item.forecast[5].low + "°" + data.query.results.channel.units.temperature + '</h4>'
+                                + '<h4>' + "High: " + data.query.results.channel.item.forecast[5].high + "°" + data.query.results.channel.units.temperature + '</h4>');
+      $('.day6-forecast').html('<h3>' + "Forecast for " +  data.query.results.channel.item.forecast[6].day + ", " + data.query.results.channel.item.forecast[1].date + ":" + '</h3>'
+                                + '<h4>' + "Current Condition: " + data.query.results.channel.item.forecast[6].text + '</h4>'
+                                + '<h4>' + "Low: " + data.query.results.channel.item.forecast[6].low + "°" + data.query.results.channel.units.temperature + '</h4>'
+                                + '<h4>' + "High: " + data.query.results.channel.item.forecast[6].high + "°" + data.query.results.channel.units.temperature + '</h4>');
       $('.result-container2').html('<p>' + "Chill: " + data.query.results.channel.wind.chill + "°" + data.query.results.channel.units.temperature + '</p>'
                                     + '<p>' + "Direction: " + data.query.results.channel.wind.direction + "°" + data.query.results.channel.units.temperature + '</p>'
                                     + '<p>' + "Speed: " + data.query.results.channel.wind.speed + data.query.results.channel.units.speed + '</p>');
