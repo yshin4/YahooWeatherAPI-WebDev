2a740ad5fd0428627f743fc0b47b5e4ddcfe8e8b
yshin4 <yshin4@lion.lmu.edu>
2016-11-01 20:00:09 -0700

    added wind and atmosphere collapse

--- yahooWeather.js
+++ yahooWeather.js
@@ -12,9 +12,14 @@ function getForecast(){
       alert("Location not found: " + location + "!");
     } else {
       $('.result-container1').html('<h2>' + data.query.results.channel.item.title + '</h2>' + data.query.results.channel.item.description);
-      $('.result-container2').html('<h2>' + data.query.results.channel.item.title + '</h2>');
-      $('.result-container3').html('<h2>' + data.query.results.channel.item.condition.temp + '</h2>');
-      $('.result-container4').html('<h2>' + "chill: " + data.query.results.channel.wind.chill + "direction: " + data.query.results.channel.wind.direction + "speed: " + data.query.results.channel.wind.speed +'</h2>');
+      $('.result-container2').html('<p>' + "Chill: " + data.query.results.channel.wind.chill + "°" + data.query.results.channel.units.temperature + '<p>'
+                                    + '<p>' + "Direction: " + data.query.results.channel.wind.direction + "°" + data.query.results.channel.units.temperature + '<p>'
+                                    + '<p>' + "Speed: " + data.query.results.channel.wind.speed + data.query.results.channel.units.speed + '<p>');
+      $('.result-container3').html('<p>' + "Humidity: " + data.query.results.channel.atmosphere.humidity + "%" + '</p>'
+                                    + '<p>' + "Visibility: " + data.query.results.channel.atmosphere.visibility + data.query.results.channel.units.distance + '</p>'
+                                    + '<p>' + "Pressure: " + data.query.results.channel.atmosphere.pressure + data.query.results.channel.units.pressure + '</p>'
+                                    + '<p>' + "Rising: " + data.query.results.channel.atmosphere.rising + '</p>');
+      $('.result-container4').html('<h2>' + data.query.results.channel.item.title + '</h2>');
       $('.result-container5').html('<h3>' + '<a href="' + data.query.results.channel.item.link + '"> Yahoo Weather Site </a>' + '</h3>');
     }
   });
