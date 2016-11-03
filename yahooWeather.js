73a2d220f532b1ef8df4ddd8067ed5e5eeec871a
yshin4 <yshin4@lion.lmu.edu>
2016-11-02 19:54:53 -0700

    fixed lines, cleaned up the code, added helped function

--- yahooWeather.js
+++ yahooWeather.js
@@ -18,26 +18,38 @@ var getForecast = function() {
             '<h4>' + "Current Condition: " + dataItem.forecast[0].text + '</h4>' + '<h4>' + "Low: " +
             dataItem.forecast[0].low + "°" +dataChannel.units.temperature + '</h4>' + '<h4>' +
             "High: " + dataItem.forecast[0].high + "°" + dataChannel.units.temperature + '</h4>');
-            $('.day1-day').html(data.query.results.channel.item.forecast[1].day);
-            $('.day2-day').html(data.query.results.channel.item.forecast[2].day);
-            $('.day3-day').html(data.query.results.channel.item.forecast[3].day);
-            $('.day4-day').html(data.query.results.channel.item.forecast[4].day);
-            $('.day5-day').html(data.query.results.channel.item.forecast[5].day);
-            $('.day6-day').html(data.query.results.channel.item.forecast[6].day);
-            $('.day1-forecast').html('<h3>' + "Forecast for " + data.query.results.channel.item.forecast[1].day + ", " + data.query.results.channel.item.forecast[1].date + ":" + '</h3>' + '<h4>' + "Current Condition: " + data.query.results.channel.item.forecast[1].text + '</h4>' + '<h4>' + "Low: " + data.query.results.channel.item.forecast[1].low + "°" + data.query.results.channel.units.temperature + '</h4>' + '<h4>' + "High: " + data.query.results.channel.item.forecast[1].high + "°" + data.query.results.channel.units.temperature + '</h4>');
-            $('.day2-forecast').html('<h3>' + "Forecast for " + data.query.results.channel.item.forecast[2].day + ", " + data.query.results.channel.item.forecast[1].date + ":" + '</h3>' + '<h4>' + "Current Condition: " + data.query.results.channel.item.forecast[2].text + '</h4>' + '<h4>' + "Low: " + data.query.results.channel.item.forecast[2].low + "°" + data.query.results.channel.units.temperature + '</h4>' + '<h4>' + "High: " + data.query.results.channel.item.forecast[2].high + "°" + data.query.results.channel.units.temperature + '</h4>');
-            $('.day3-forecast').html('<h3>' + "Forecast for " + data.query.results.channel.item.forecast[3].day + ", " + data.query.results.channel.item.forecast[1].date + ":" + '</h3>' + '<h4>' + "Current Condition: " + data.query.results.channel.item.forecast[3].text + '</h4>' + '<h4>' + "Low: " + data.query.results.channel.item.forecast[3].low + "°" + data.query.results.channel.units.temperature + '</h4>' + '<h4>' + "High: " + data.query.results.channel.item.forecast[3].high + "°" + data.query.results.channel.units.temperature + '</h4>');
-            $('.day4-forecast').html('<h3>' + "Forecast for " + data.query.results.channel.item.forecast[4].day + ", " + data.query.results.channel.item.forecast[1].date + ":" + '</h3>' + '<h4>' + "Current Condition: " + data.query.results.channel.item.forecast[4].text + '</h4>' + '<h4>' + "Low: " + data.query.results.channel.item.forecast[4].low + "°" + data.query.results.channel.units.temperature + '</h4>' + '<h4>' + "High: " + data.query.results.channel.item.forecast[4].high + "°" + data.query.results.channel.units.temperature + '</h4>');
-            $('.day5-forecast').html('<h3>' + "Forecast for " + data.query.results.channel.item.forecast[5].day + ", " + data.query.results.channel.item.forecast[1].date + ":" + '</h3>' + '<h4>' + "Current Condition: " + data.query.results.channel.item.forecast[5].text + '</h4>' + '<h4>' + "Low: " + data.query.results.channel.item.forecast[5].low + "°" + data.query.results.channel.units.temperature + '</h4>' + '<h4>' + "High: " + data.query.results.channel.item.forecast[5].high + "°" + data.query.results.channel.units.temperature + '</h4>');
-            $('.day6-forecast').html('<h3>' + "Forecast for " + data.query.results.channel.item.forecast[6].day + ", " + data.query.results.channel.item.forecast[1].date + ":" + '</h3>' + '<h4>' + "Current Condition: " + data.query.results.channel.item.forecast[6].text + '</h4>' + '<h4>' + "Low: " + data.query.results.channel.item.forecast[6].low + "°" + data.query.results.channel.units.temperature + '</h4>' + '<h4>' + "High: " + data.query.results.channel.item.forecast[6].high + "°" + data.query.results.channel.units.temperature + '</h4>');
-            $('.result-container2').html('<p>' + "Chill: " + data.query.results.channel.wind.chill + "°" + data.query.results.channel.units.temperature + '</p>' + '<p>' + "Direction: " + data.query.results.channel.wind.direction + "°" + data.query.results.channel.units.temperature + '</p>' + '<p>' + "Speed: " + data.query.results.channel.wind.speed + data.query.results.channel.units.speed + '</p>');
-            $('.result-container3').html('<p>' + "Humidity: " + data.query.results.channel.atmosphere.humidity + "%" + '</p>' + '<p>' + "Visibility: " + data.query.results.channel.atmosphere.visibility + data.query.results.channel.units.distance + '</p>' + '<p>' + "Pressure: " + data.query.results.channel.atmosphere.pressure + data.query.results.channel.units.pressure + '</p>' + '<p>' + "Rising: " + data.query.results.channel.atmosphere.rising + '</p>');
+            $('.day1-day').html(dataItem.forecast[1].day);
+            $('.day2-day').html(dataItem.forecast[2].day);
+            $('.day3-day').html(dataItem.forecast[3].day);
+            $('.day4-day').html(dataItem.forecast[4].day);
+            $('.day5-day').html(dataItem.forecast[5].day);
+            $('.day6-day').html(dataItem.forecast[6].day);
+            $('.day1-forecast').html(getForecastByDay(1,dataItem,dataChannel));
+            $('.day2-forecast').html(getForecastByDay(2,dataItem,dataChannel));
+            $('.day3-forecast').html(getForecastByDay(3,dataItem,dataChannel));
+            $('.day4-forecast').html(getForecastByDay(4,dataItem,dataChannel));
+            $('.day5-forecast').html(getForecastByDay(5,dataItem,dataChannel));
+            $('.day6-forecast').html(getForecastByDay(6,dataItem,dataChannel));
+            $('.result-container2').html('<p>' + "Chill: " + dataChannel.wind.chill + "°" + dataChannel.units.temperature + '</p>' +
+            '<p>' + "Direction: " + dataChannel.wind.direction + "°" + dataChannel.units.temperature + '</p>' +
+            '<p>' + "Speed: " + dataChannel.wind.speed + dataChannel.units.speed + '</p>');
+            $('.result-container3').html('<p>' + "Humidity: " + dataChannel.atmosphere.humidity + "%" + '</p>' +
+            '<p>' + "Visibility: " + dataChannel.atmosphere.visibility + dataChannel.units.distance + '</p>' +
+            '<p>' + "Pressure: " + dataChannel.atmosphere.pressure + dataChannel.units.pressure + '</p>' +
+            '<p>' + "Rising: " + dataChannel.atmosphere.rising + '</p>');
             $('.result-container4').html('<h2>' + '<img src="Sunrise.svg">' + 'Sunrise: ' + data.query.results.channel.astronomy.sunrise + '</h2>');
             $('.result-container5').html('<h2>' + '<img src="Sunrise.svg">' + 'Sunset: ' + data.query.results.channel.astronomy.sunset + '</h2>');
-            $('.result-container6').html('<h3>' + '<a href="' + data.query.results.channel.item.link + '"> Yahoo Weather Site </a>' + '</h3>' + '<h3>' + "latitude " + data.query.results.channel.item.lat + " longitide " + data.query.results.channel.item.long + '</h3>');
+            $('.result-container6').html('<h3>' + '<a href="' + dataItem.link + '"> Yahoo Weather Site </a>' + '</h3>' +
+            '<h3>' + "latitude " + dataItem.lat + '</h3>' + '<h3>' + " longitide " + dataItem.long + '</h3>');
         }
     });
     return false;
+};
+var getForecastByDay = function(i, dataItem, dataChannel){
+  return ('<h3>' + "Forecast for " + dataItem.forecast[i].day + ", " + dataItem.forecast[i].date + ":" + '</h3>' +
+  '<h4>' + "Current Condition: " + dataItem.forecast[i].text + '</h4>' +
+  '<h4>' + "Low: " + dataItem.forecast[i].low + "°" + dataChannel.units.temperature + '</h4>' +
+  '<h4>' + "High: " + dataItem.forecast[i].high + "°" + dataChannel.units.temperature + '</h4>');
 };
 $(document).ready(function() {
     $(".nav-tabs a").click(function() {
